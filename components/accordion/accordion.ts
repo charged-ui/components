b8241d89cd44588257f64d5c94966e325b570f8e
themeboutique <noreply@charged.dev>
2023-12-27 08:29:22 -0500

    Implement prettier/eslint

--- components/accordion/accordion.ts
+++ components/accordion/accordion.ts
@@ -1,41 +1,41 @@
 import './accordion.css';
 
 class ChargedAccordion extends HTMLElement {
-	constructor() {
-		super();
-	}
+  constructor() {
+    super();
+  }
 
-	// Called when the element is added to the DOM.
-	connectedCallback() {
-		this.addEventListener('toggle', this.handleToggle, true);
-	}
+  // Called when the element is added to the DOM.
+  connectedCallback() {
+    this.addEventListener('toggle', this.handleToggle, true);
+  }
 
-	// Called when the element is removed from the DOM.
-	disconnectedCallback() {
-		this.removeEventListener('toggle', this.handleToggle, true);
-	}
+  // Called when the element is removed from the DOM.
+  disconnectedCallback() {
+    this.removeEventListener('toggle', this.handleToggle, true);
+  }
 
-	expand() {}
+  expand() {}
 
-	collapse() {}
+  collapse() {}
 
-	// Called when a ToggleEvent occurs
-	handleToggle(e: Event) {
-		// Target element
-		const target = e.target as HTMLDetailsElement;
+  // Called when a ToggleEvent occurs
+  handleToggle(e: Event) {
+    // Target element
+    const target = e.target as HTMLDetailsElement;
 
-		// If accordion is closed, stop execution
-		if (!target?.open) return;
+    // If accordion is closed, stop execution
+    if (!target?.open) return;
 
-		// Find open accordion items
-		const open = this.querySelectorAll('details[open]');
+    // Find open accordion items
+    const open = this.querySelectorAll('details[open]');
 
-		// Close previously open accordion items
-		for (const child of open) {
-			if (child === target) continue;
-			child.removeAttribute('open');
-		}
-	}
+    // Close previously open accordion items
+    for (const child of open) {
+      if (child === target) continue;
+      child.removeAttribute('open');
+    }
+  }
 }
 
 customElements.define('ui-accordion', ChargedAccordion);
