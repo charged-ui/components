07d8bdb9fa39ba390f85a33b54fcc3529ef8c2e1
themeboutique <noreply@charged.dev>
2024-02-18 11:44:50 -0500

    Add Charged UI logo

--- docs/src/stories/Button.ts
+++ docs/src/stories/Button.ts
@@ -27,13 +27,25 @@ export interface ButtonProps {
 /**
  * Primary UI component for user interaction
  */
-export const Button = ({ primary, backgroundColor, size, label, onClick }: ButtonProps) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+export const Button = ({
+  primary,
+  backgroundColor,
+  size,
+  label,
+  onClick
+}: ButtonProps) => {
+  const mode = primary
+    ? 'storybook-button--primary'
+    : 'storybook-button--secondary';
 
   return html`
     <button
       type="button"
-      class=${['storybook-button', `storybook-button--${size || 'medium'}`, mode].join(' ')}
+      class=${[
+        'storybook-button p-4',
+        `storybook-button--${size || 'medium'}`,
+        mode
+      ].join(' ')}
       style=${styleMap({ backgroundColor })}
       @click=${onClick}
     >
