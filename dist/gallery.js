f95e28f36d69c2f2c6fab46e33dbd2ea1cae7350
Mitch Kostelecky <noreply@charged.dev>
2025-03-30 09:03:45 -0400

    Extract motion from vendor (#12)

--- dist/gallery.js
+++ dist/gallery.js
@@ -1,5 +1,6 @@
-import { i as m, r as g, n as p, a as w, h, b as u, s as b, k as d, t as f } from "./vendor.js";
+import { i as m, r as g, n as p, a as w, h, k as d, t as u } from "./vendor.js";
 import "./button.js";
+import { a as f, s as b } from "./motion.js";
 const y = "*,::backdrop,:after,:before{--tw-border-spacing-x:0;--tw-border-spacing-y:0;--tw-translate-x:0;--tw-translate-y:0;--tw-rotate:0;--tw-skew-x:0;--tw-skew-y:0;--tw-scale-x:1;--tw-scale-y:1;--tw-pan-x: ;--tw-pan-y: ;--tw-pinch-zoom: ;--tw-scroll-snap-strictness:proximity;--tw-gradient-from-position: ;--tw-gradient-via-position: ;--tw-gradient-to-position: ;--tw-ordinal: ;--tw-slashed-zero: ;--tw-numeric-figure: ;--tw-numeric-spacing: ;--tw-numeric-fraction: ;--tw-ring-inset: ;--tw-ring-offset-width:0px;--tw-ring-offset-color:#fff;--tw-ring-color:#3b82f680;--tw-ring-offset-shadow:0 0 #0000;--tw-ring-shadow:0 0 #0000;--tw-shadow:0 0 #0000;--tw-shadow-colored:0 0 #0000;--tw-blur: ;--tw-brightness: ;--tw-contrast: ;--tw-grayscale: ;--tw-hue-rotate: ;--tw-invert: ;--tw-saturate: ;--tw-sepia: ;--tw-drop-shadow: ;--tw-backdrop-blur: ;--tw-backdrop-brightness: ;--tw-backdrop-contrast: ;--tw-backdrop-grayscale: ;--tw-backdrop-hue-rotate: ;--tw-backdrop-invert: ;--tw-backdrop-opacity: ;--tw-backdrop-saturate: ;--tw-backdrop-sepia: ;--tw-contain-size: ;--tw-contain-layout: ;--tw-contain-paint: ;--tw-contain-style: }.\\!visible{visibility:visible!important}.visible{visibility:visible}.static{position:static}.mb-8{margin-bottom:2rem}.flex{display:flex}.grid{display:grid}.hidden{display:none}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}.flex-wrap{flex-wrap:wrap}.gap-2{gap:.5rem}.gap-6{gap:1.5rem}.rounded{border-radius:.25rem}.filter{filter:var(--tw-blur) var(--tw-brightness) var(--tw-contrast) var(--tw-grayscale) var(--tw-hue-rotate) var(--tw-invert) var(--tw-saturate) var(--tw-sepia) var(--tw-drop-shadow)}@media (min-width:640px){.sm\\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}";
 var v = Object.defineProperty, x = Object.getOwnPropertyDescriptor, n = (t, e, i, s) => {
   for (var r = s > 1 ? void 0 : s ? x(e, i) : e, a = t.length - 1, l; a >= 0; a--)
@@ -52,7 +53,7 @@ let o = class extends h {
   animateItems() {
     var e, i;
     const t = (i = (e = this.shadowRoot) == null ? void 0 : e.querySelector("slot")) == null ? void 0 : i.assignedElements().filter((s) => !s.classList.contains("hidden"));
-    t != null && t.length && u(
+    t != null && t.length && f(
       t,
       { opacity: [0, 1], scale: [0.8, 1] },
       { delay: b(0.05, { ease: [0.4, 0, 0.2, 1] }) }
@@ -108,7 +109,7 @@ n([
   w()
 ], o.prototype, "filters", 2);
 o = n([
-  f("ui-gallery")
+  u("ui-gallery")
 ], o);
 export {
   o as UIGallery
