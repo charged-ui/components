df71c7d110f0e6f4dfde0d6f3d4466b9682f25c5
themeboutique <noreply@charged.dev>
2025-06-19 22:20:07 -0400

    Remove deps from icon-selector

--- src/components/icon-selector/icon-selector.ts
+++ src/components/icon-selector/icon-selector.ts
@@ -1,9 +1,6 @@
 import { LitElement, html, css, unsafeCSS } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 import styles from './icon-selector.css?raw';
-import '../icon/icon';
-import '../button/button';
-import '../modal/modal';
 
 // Define interface for icon objects
 interface HeroIcon {
@@ -135,10 +132,7 @@ export class UIIconSelector extends LitElement {
                 class="flex items-center justify-center w-12 h-12 border border-dashed border-zinc-200 rounded-xl hover:bg-emerald-100 cursor-pointer"
                 @click=${() => this.handleIconSelect(icon.name)}
               >
-                <ui-icon
-                  name="${icon.name}"
-                  variant="${this.variant}"
-                ></ui-icon>
+                <ui-icon name="${icon.name}" variant="${this.variant}" />
               </div>`
           )}
         </div>
